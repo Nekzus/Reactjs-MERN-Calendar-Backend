87e0f024de9a755ee10970a8edeb69d98aa154c5
Nekzus <85507589+Nekzus@users.noreply.github.com>
2022-07-19 22:50:33 -0300

    Implementacion de express

--- routes/auth.js
+++ routes/auth.js
@@ -4,13 +4,39 @@ host + /api/auth
  */
 
 const { Router } = require("express");
+const { check } = require("express-validator");
 
 const router = Router();
+const {
+  crearUsuario,
+  loginUsuario,
+  revalidarToken,
+} = require("../controllers/auth");
 
-router.get("/", (req, res) => {
-  res.json({
-    ok: true,
-  });
-});
+router.post(
+  "/new",
+  [
+    // Middlewares
+    check("name", "El nombre es obligatorio").not().isEmpty(),
+    check("email", "El email es obligatorio").isEmail(),
+    check("password", "El password debe de ser de 6 caracteres").isLength({
+      min: 6,
+    }),
+  ],
+  crearUsuario
+);
+
+router.post(
+  "/",
+  [
+    check("email", "El email es obligatorio").isEmail(),
+    check("password", "El password debe de ser de 6 caracteres").isLength({
+      min: 6,
+    }),
+  ],
+  loginUsuario
+);
+
+router.get("/renew", revalidarToken);
 
 module.exports = router;
